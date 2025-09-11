d9afec9aa5337d13d50efa5a8ace84e51b950bc3
Pablo Perez <pablo.perezm@upb.edu.co>
2025-09-11 11:39:51 -0500

    reorganizacion

--- Reto.py
+++ Reto.py
@@ -5,15 +5,25 @@
 
 ##1 regulación de la temperatura en el baúl del tren de aterrizaje dependiendo progresivamente de la temperatura de las llantas 
 '''
-necesitamos controlar la temperatura en las llantas ya que en partes puede ocurrir de que se enfrie demasiado o que se caliente demasiado entonces respecto a esta formula
+necesitamos controlar la temperatura en las llantas durante el vuelo debido a que despues del despegue, las ruedas del avión
+quedan muy calientes debido a la fricción de la pista, durante el vuelo, la temperatura de refrigeracion del compartimiento del tren de aterrizaje
+va a estar cambiando cada 5 minutos para que la temperatura de las ruedas este oscilando, con esta formula,
+vamos a poder definir cuanta temperatura puede cambiar (aumentar o disminuir) con respecto a la temperatura del tren de aterrizaje despues del despegue
 dT/dt = -k * (T - Te)
 dT/dt es la tasa de cambio de temperatura del objeto respecto al tiempo.
 k es la constante de enfriamiento o calentamiento, que depende de las propiedades del objeto y del medio en el que se encuentra.
 T es la temperatura del objeto en un momento dado.
 Te es la temperatura del entorno o medio en el que se encuentra el objeto.
-respecto a esto necesitamos que despues del despegue analizar constatntemente cada 10 min la temperatura y corregirla si es necesario
-si la temperatura marca un nivel distinto al momento de ejecutar la accion debe mostrar una alerta en cabina respecto a la situacion, y necesitamos
-que este estable 5 minutas antes del aterrizaje. en el intervalo de tiempo despues del aterrizaje hasta 5 min antes de aterrizar, la temperatura
+la constante k solo tendra 3 valores propuedos respecto a los aviones
+k para el Airbus A380-800 = 8
+k para  el Boeing 747-8 = 5
+k para el Antonov An-225 = 10
+respecto a esto necesitamos corregirla
+en caso de que la temperatura sobre pase un limite por las oscilaciones ( demasiado bajo o demasiado alto) ordenar al piloto active el sistema de refrigeración controlado, en el cual el piloto podra escoger
+las temperaturas ideales del sistema de refrigeracion para que enfrie o caliente con mayor intensidad. en caso de activar este sistema, se mostrará un mensaje con las regulaciones ideales
+debido a que si el tren de aterrizaje esta muy frio, no puede recibir una temperatura muy caliente en el instante, o si esta muy caliente, no puede recibir una temperatura muy fria por parte del sistema de refrigeracion
+regulado, debido a que se puede dañar el caucho de las ruedas del tren de aterrizaje, en caso de que se escoja una temperatura ideal que lo dañe, mostrar un error y el piloto debera escoger otra temperatura ideal
+que este estable 10 minutas antes del aterrizaje. en el intervalo de tiempo despues del aterrizaje hasta 5 min antes de aterrizar, la temperatura
 de las ruedas este oscilando entre frio o caliente
 '''
 ##2 como segudo caso tenemos la presion dentro de cabina  que varia dependiendo la altitud
@@ -23,19 +33,7 @@ Durante el ascenso La presión en cabina empieza a disminuir, pero de forma más
 En crucero entre 10.000 y 12.000 m de altura  La cabina suele mantenerse equivalente a estar entre 1.500 y 2.400 m
 de altitud aproximadamente 10.9 y 11.6 psi
 Durante el descenso  La presión en cabina aumenta suavemente hasta igualar la exterior en el aterrizaje.
+En este caso debemos calcular constantemente una regulación del aire y de presión en cabuina para que la misma se mantenga presurizada
+y pueda resitir la presión exterior.
 '''
 
-##3 como ultimo caso tenemos el medidor de gasolina que es el que nos muestra cuanto nivel tenemos, y si es necesario recargar gaspolina
-'''
-necesitamos saber cuanto combustibles tiene el avion en vuelo constantemente, y a partir de su distancia, demostrar si si alcanza para el destino propuesto, en caso de 
-de que no, hacer un aterrizaje de emergencia 
- '''
-
-'''
-1 regulación de la temperatura en el baúl del tren de aterrizaje dependiendo progresivamente de la temperatura de las llantas
-Crear una temperatura estable en ela cual se regule la temperatura de el tren de aterrizaje, debido a que el tren de aterrizaje queda con mucho calor despues de el despegue,
-necesitas un bucle para poder tener la temperatura de las ruedas estables (ni muy caliente, ni muy fria), la temperatura debe estar cambiando constantemente
-para que esten estables, teniedno en cuenta que el baul de el tren de aterrizaje, tambien genera calor y necesita ventilación.
-dT/dt = -k * (T - Te) Donde: dT/dt es la tasa de cambio de temperatura del objeto respecto al tiempo. k es la constante de enfriamiento
-'''
-rn**r-1
---
+++ tabal_evaluacion.md
@@ -0,0 +1,11 @@
+| Requisito | cumple 2, parcialmente no cumple 1, no complw 0   |   evidencia (paginal/tabla/figura/seccion)    |
+|-----------|---------------------------------------------------|-----------------------------------------------|
+|Contexto aeronáutico claro y relevante|-------------------------|
+|Clara definición y clasificación de las variables de entrada, salida, de control e intermedias|--------------|
+|Ecuación que relaciona adecuadamente las variables del problema|----------------|
+|No es solo cálculo directo|------------------------|
+|Al menos un bucle (variable de control, condición de parada)|-------------------|
+|Al menos una sentencia condicional significativa|----------------------|
+|Menú repetitivo hasta “Salir”|----------------------|
+|Sin listas, diccionarios, tuplas ni sets|--------------------|
+|Declaración de uso de IA (si aplica)|------------------------|
